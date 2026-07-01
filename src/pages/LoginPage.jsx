ac8e2cb3128b6598ee30e394ec3e1b7bb9716a14
YenaKC <yenakim242@icloud.com>
2026-07-01 17:47:28 +0200

    Add login page and product details cart action

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+
 function LoginPage() {
+    // Email + Password => Loggin Backend => Save JWT Token
+    // useState: Save the data from email/password
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    // To not to execute refresh when the form is submitted
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        api
+            .post("/auth/login", { email, password })
+            .then((res) => {
+                // Save authToken to localStorage
+                localStorage.setItem("authToken", res.data.authToken);
+                // Move to /products
+                navigate("/products");
+            })
+            .catch(console.log);
+    };
+
     return (
         <main className="page">
-            <div className="container">
+            <div className="container auth-container">
                 <h1>FIT MAJOR</h1>
-                <p>Login Page</p>
+                <p className="auth-subtitle">LOGIN</p>
+
+                <form onSubmit={handleSubmit} className="auth-form">
+                    <label>Email</label>
+                    <input 
+                        className="input"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <label>Password</label>
+                    <input 
+                        className="input"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button className="btn"type="submit">
+                        LOGIN
+                    </button>
+                </form>
+
+                <Link to="/signup" className="auth-link">
+                    Create account
+                </Link>
             </div>
         </main>
     );
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,68 @@
+import { useEffect, useState } from "react";
+// useParams: To bring the productID from URL
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../services/api";
+
 function ProductDetailsPage() {
+    const { productId } = useParams();
+    const [product, setProduct] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleAddToCart = () => {
+        const token = localStorage.getItem("authToken");
+
+        api
+            .post(
+                "/cart/add",
+                {
+                    productId: product._id,
+                    quantity: 1,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            )
+            .then(() => {
+                navigate("/cart");
+            })
+            .catch(console.log);
+        };
+
+
+    useEffect(() => {
+        api
+            .get(`/products/${productId}`) // Bring one product from the back-end server
+            .then((res) => {
+                setProduct(res.data); //Save at the state
+            })
+            .catch(console.log);
+    }, [productId]);
+
+    if (!product) {
+        return (
+            <main className="page">
+                <div className="container">
+                    <p>Loading product...</p>
+                </div>
+            </main>
+        );
+    }
+
     return (
         <main className="page">
-            <div className="container">
-                <h1>FIT MAJOR</h1>
-                <p>Product Details Page</p>
-            </div>
+            <img src={product.image} alt={product.name} className="pdp-image" />
+
+            <section className="pdp-info">
+                <p className="badge">{product.category}</p>
+                <h1>{product.name}</h1>
+                <h2>{product.price}€</h2>
+                <p>{product.description}</p>
+
+                <button className="btn" onClick={handleAddToCart}>ADD TO BAG</button>
+            </section>
         </main>
     );
 }
