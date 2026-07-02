a24711b857cd36a45f38de0c7bfd2026dd91a42f
YenaKC <yenakim242@icloud.com>
2026-07-02 17:28:30 +0200

    Add vercel configuration

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 // const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-    
+    baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
 export default api;
